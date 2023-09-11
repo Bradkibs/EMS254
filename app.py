c14f3f58dc57ece4da51fc20a01e46fca2c1f357
Bradkibs <Bradleykibs@gmail.com>
2023-09-11 22:34:30 +0300

    setting up project essentials

---
+++ app.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python3
+"""
+Flask App sends and accept json api requests to the set frontend
+"""
+
+
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS, cross_origin
+from flasgger import Swagger
+import os
+from werkzeug.exceptions import HTTPException
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+app = Flask(__name__)
+
+app.url_map.strict_slashes = False
+
+host = os.getenv("APP_HOST", "0.0.0.0")
+port = os.getenv("APP_PORT", 5000)
+
+cors = CORS(app, origins="0.0.0.0")
+cors = CORS(app, resources={r'/*': {'origins': host}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+@app.route("/")
+def home():
+    return jsonify("Hello world"), 200
+
+# begin flask page rendering
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    after each request, this method calls .close() (i.e. .remove()) on
+    the current SQLAlchemy Session
+    """
+    storage.close()
+
+
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    handles 404 errors, in the event that global error handler fails
+    """
+    code = exception.__str__().split()[0]
+    description = exception.description
+    message = {'error': description}
+    return make_response(jsonify(message), code)
+
+
+@app.errorhandler(400)
+def handle_404(exception):
+    """
+    handles 400 errors, in the event that global error handler fails
+    """
+    code = exception.__str__().split()[0]
+    description = exception.description
+    message = {'error': description}
+    return make_response(jsonify(message), code)
+
+
+@app.errorhandler(Exception)
+def global_error_handler(err):
+    """
+        Global Route to handle All Error Status Codes
+    """
+    if isinstance(err, HTTPException):
+        if type(err).__name__ == 'NotFound':
+            err.description = "Not found"
+        message = {'error': err.description}
+        code = err.code
+    else:
+        message = {'error': err}
+        code = 500
+    return make_response(jsonify(message), code)
+
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers.extend({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Headers': 'Content-Type, Cache-Control, X-Requested-With, Authorization',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, DELETE'
+    })
+    return response
+
+
+def setup_global_errors():
+    """
+    This updates HTTPException Class with custom error function
+    """
+    for cls in HTTPException.__subclasses__():
+        app.register_error_handler(cls, global_error_handler)
+
+
+if __name__ == "__main__":
+    """
+    MAIN Flask App
+    """
+    # initializes global error handling
+    setup_global_errors()
+    # start Flask app
+    app.run(host=host, port=port, debug=True)
--- db/db.py
+++ db/db.py
@@ -2,7 +2,12 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
 from os import getenv
+from dotenv import load_dotenv
 from models.user import Base
+
+
+load_dotenv()
+
 
 class DB:
     """
@@ -17,13 +22,20 @@ class DB:
         Constructor
         """
 
-        user = getenv('USER')
-        password = getenv('PASSWORD')
-        host = getenv('HOST')
-        db_name = getenv('DATABASE')
+        user = getenv('MYSQL_USER')
+        password = getenv('MYSQL_PWD')
+        host = getenv('MYSQL_HOST')
+        db_name = getenv('MYSQL_DB')
+        env = getenv('APP_ENV')
 
+        try:
+            self.__engine = create_engine(f'mysql+mysqldb://{user}:{password}@{host}/{db_name}')
+            self.reload()
 
-        self.__engine = create_engine(f'mysql+mysqldb://{user}:{password}@{host}/{db_name}')
+            if env == 'test':
+                Base.metadata.drop_all(self.__engine)
+        except SQLAlchemyError as e:
+            raise e
 
 
     def reload(self):
