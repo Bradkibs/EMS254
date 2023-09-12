5ec97a778c8f63f42e3304da5c4155c223abc0e4
Bradleykibs <Bradleykibwana@gmail.com>
2023-09-13 00:08:21 +0300

    implemented jwt authentication class

---
+++ auth/auth.py
@@ -0,0 +1,31 @@
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, unset_jwt_cookies, \
+    set_access_cookies, verify_jwt_in_request
+
+
+class Authentication:
+
+    __token = None
+
+    def create_token(self, identity):
+        self.__token = create_access_token(identity=identity)
+        return self.__token
+
+    def refresh_token(self, identity):
+        self.create_token(identity)
+
+    def validate_jwt(self):
+        try:
+            verify_jwt_in_request()
+        except Exception as e:
+            return False
+        return True
+    def get_authenticated_user(self):
+        if self.validate_jwt():
+            return get_jwt_identity()
+        return None
+
+    def set_cookie(self, response, access_token):
+        set_access_cookies(response)
+
+    def unset_cookie(self, response):
+        unset_jwt_cookies(response)
