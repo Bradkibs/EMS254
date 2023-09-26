dcb4df8bfb6a7fcaebe724bac87a22c786a52497
Bradleykibs <Bradleykibwana@gmail.com>
2023-09-26 18:58:08 +0300

    fix

--- app.py
+++ app.py
@@ -141,9 +141,10 @@ def global_error_handler(err):
 
 @app.after_request
 def add_cors_headers(response):
+    frontend_url ="https://74bc-41-90-70-217.ngrok-free.app/"
     response.headers.extend({
         'X-Content-Type-Options': 'nosniff',
-        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': frontend_url,
         'Access-Control-Allow-Credentials': 'true',
         'Access-Control-Allow-Headers': 'Content-Type, Cache-Control, X-Requested-With, Authorization',
         'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, DELETE'
